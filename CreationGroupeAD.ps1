4d90eae7ded3aaeb28a88a50ec70da785e0988eb
Francois-Xavier Berard <swipe.unbound@gmail.com>
2022-04-23 08:44:53 -0400

    Update CreationGroupeAD.ps1

--- CreationGroupeAD.ps1
+++ CreationGroupeAD.ps1
@@ -1,7 +1,7 @@
-﻿ Write-Host "Bienvenue dans l'utilitaire de création de groupe du domaine" -ForegroundColor Green
+ Write-Host "Bienvenue dans l'utilitaire de création de groupe du domaine" -ForegroundColor Green
 $name= Read-Host -Prompt "Entrez le nom du groupe"
 $gpcat= Read-Host -Prompt "Entrez la catégorie (Security/Distribution)"
-$gpscope= Read-Host -Prompt "Entrez le type de groupe (Global/DomainLocal)"
+$gpscope= Read-Host -Prompt "Entrez le type de groupe (Global/Universal/DomainLocal)"
 $description= Read-Host -Prompt "Entrez une description du groupe ou laissez vide"
 New-ADGroup `
 -Name "$name"`
