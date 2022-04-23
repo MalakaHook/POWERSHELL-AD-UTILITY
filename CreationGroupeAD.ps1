c9406669986b3d765cb52c03577317bb007076f0
Francois-Xavier Berard <swipe.unbound@gmail.com>
2022-04-23 08:40:16 -0400

    Add files via upload

---
+++ CreationGroupeAD.ps1
@@ -0,0 +1,10 @@
+﻿ Write-Host "Bienvenue dans l'utilitaire de création de groupe du domaine" -ForegroundColor Green
+$name= Read-Host -Prompt "Entrez le nom du groupe"
+$gpcat= Read-Host -Prompt "Entrez la catégorie (Security/Distribution)"
+$gpscope= Read-Host -Prompt "Entrez le type de groupe (Global/DomainLocal)"
+$description= Read-Host -Prompt "Entrez une description du groupe ou laissez vide"
+New-ADGroup `
+-Name "$name"`
+-GroupCategory "$gpcat"`
+-GroupScope "$gpscope"`
+-Description "$description"
---
+++ SuppressionUserAD.ps1
@@ -0,0 +1,3 @@
+﻿Write-Host "Bienvenue dans l'utilitaire de suppression d'utilisateur AD créer par Francois-Xavier Bérard" -ForegroundColor Red
+$SAM= Read-Host -Prompt "Entrez SamAccountName (Non utilisé pour la connection) que vous désirez supprimer"
+Remove-ADUser -Identity "$SAM"
